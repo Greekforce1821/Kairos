9ff32e5cb0f0c009e38bd1d2763ca363ae95c460
Greekforce1821 <spiroskoeu@gmail.com>
2022-12-30 15:46:30 +0200

    Added comments ...

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+# Important Libraries That The Program Uses
 
 from tkinter import *
 import tkinter as tk
@@ -9,7 +10,7 @@ import requests
 import pytz
 from PIL import Image, ImageTk
 
-
+# Important Information About The GUI
 
 root=Tk()
 root.title("Kairos Weather App")
@@ -17,10 +18,12 @@ root.geometry("890x470+300+300")
 root.configure(bg="#57adff")
 root.resizable(False, False)
 
-
+# Images For The GUI
 
 image_icon=PhotoImage(file="images/logo.png")
 root.iconphoto(False, image_icon)
+
+# Assets For The GUI
 
 Round_box=PhotoImage(file="images/rr1.png")
 Label(root, image= Round_box, bg="#57adff").place(x=30, y=110)
