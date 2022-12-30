a1d7ff658df7dc5cc325ab790d16d93d1772557a
Greekforce1821 <spiroskoeu@gmail.com>
2022-12-30 16:14:45 +0200

    added support for search box...

--- src/main.py
+++ src/main.py
@@ -27,3 +27,30 @@ root.iconphoto(False, image_icon)
 
 Round_box=PhotoImage(file="images/rr1.png")
 Label(root, image= Round_box, bg="#57adff").place(x=30, y=110)
+
+# Added Labels For Each Temperature
+
+label_one=Label(root, text="Temperature", font= ('Helvetica', 11), fg="white", bg="#203243")
+label_one.place(x=50, y=120)
+
+label_two=Label(root, text="Humidity", font= ('Helvetica', 11), fg="white", bg="#203243")
+label_two.place(x=50, y=120)
+
+label_three=Label(root, text="Pressure", font= ('Helvetica', 11), fg="white", bg="#203243")
+label_three.place(x=50, y=120)
+
+label_four=Label(root, text="Wind Speed", font= ('Helvetica', 11), fg="white", bg="#203243")
+label_four.place(x=50, y=120)
+
+label_fifth=Label(root, text="Description", font= ('Helvetica', 11), fg="white", bg="#203243")
+label_fifth.place(x=50, y=120)
+
+
+# Including A Search Box For The Cities
+
+Search_image=PhotoImage(file="images/rr3.png")
+image_me=Label(image=Search_image, bg="#57adff")
+image_me.place(x=270, y=120)
+cloud_image=PhotoImage(file="images/clouds.png")
+weather_image=Label(root, image=cloud_image, bg="#203243")
+weather_image.place(x=290, y=12)
