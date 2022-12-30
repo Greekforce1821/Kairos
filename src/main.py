5f33fc68f0c74b0ce6672edea3aea8bd33957314
Greekforce1821 <spiroskoeu@gmail.com>
2022-12-30 16:51:16 +0200

    added footers for the GUI...

--- src/main.py
+++ src/main.py
@@ -34,26 +34,26 @@ label_one=Label(root, text="Temperature", font= ('Helvetica', 11), fg="white", b
 label_one.place(x=50, y=120)
 
 label_two=Label(root, text="Humidity", font= ('Helvetica', 11), fg="white", bg="#203243")
-label_two.place(x=50, y=120)
+label_two.place(x=50, y=140)
 
 label_three=Label(root, text="Pressure", font= ('Helvetica', 11), fg="white", bg="#203243")
-label_three.place(x=50, y=120)
+label_three.place(x=50, y=160)
 
 label_four=Label(root, text="Wind Speed", font= ('Helvetica', 11), fg="white", bg="#203243")
-label_four.place(x=50, y=120)
+label_four.place(x=50, y=180)
 
 label_fifth=Label(root, text="Description", font= ('Helvetica', 11), fg="white", bg="#203243")
-label_fifth.place(x=50, y=120)
+label_fifth.place(x=50, y=200)
 
 
 # Including A Search Box For The Cities
 
 Search_image=PhotoImage(file="images/rr3.png")
-image_me=Label(image=Search_image, bg="#57adff")
-image_me.place(x=270, y=120)
+image_search=Label(image=Search_image, bg="#57adff")
+image_search.place(x=270, y=120)
 cloud_image=PhotoImage(file="images/clouds.png")
 weather_image=Label(root, image=cloud_image, bg="#203243")
-weather_image.place(x=290, y=12)
+weather_image.place(x=290, y=127)
 
 # Including Text Input For The Search Box
 
@@ -61,4 +61,58 @@ textfield=tk.Entry(root, justify='center', width=15, font=('poppins', 25, 'bold'
 textfield.place(x=370, y=130)
 textfield.focus()
 
-#
+# Including A Magnifier As A Photo Of The Search Box
+
+Search_icon=PhotoImage(file="images/search-icon.png")
+image_icon=Button(image=Search_icon, borderwidth=0, cursor="hand2", bg="#203243")
+image_icon.place(x=645, y=125)
+
+
+# Adding Support For Footer Boxes That They Will Be Used For Representing Data From The Forecast
+
+footer_frame=Frame(root, width=900, height=180, bg="#212120")
+footer_frame.pack(side=BOTTOM)
+
+footer_box_one=PhotoImage(file="images/rr2.png")
+footer_box_two=PhotoImage(file="images/rr2c.png")
+footer_box_three=PhotoImage(file="images/rr2c.png")
+footer_box_four=PhotoImage(file="images/rr2c.png")
+footer_box_five=PhotoImage(file="images/rr2c.png")
+footer_box_six=PhotoImage(file="images/rr2c.png")
+footer_box_seven=PhotoImage(file="images/rr2c.png")
+
+# Each Footer Represents A Label At The Bottom Of The GUI
+
+Label(footer_frame, image=footer_box_one, bg="#212120").place(x=30, y=20)
+Label(footer_frame, image=footer_box_two, bg="#212120").place(x=300, y=20)
+Label(footer_frame, image=footer_box_three, bg="#212120").place(x=400, y=20)
+Label(footer_frame, image=footer_box_four, bg="#212120").place(x=500, y=20)
+Label(footer_frame, image=footer_box_five, bg="#212120").place(x=600, y=20)
+Label(footer_frame, image=footer_box_six, bg="#212120").place(x=700, y=20)
+Label(footer_frame, image=footer_box_seven, bg="#212120").place(x=800, y=20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
